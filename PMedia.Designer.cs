3d5b6b2d148edaf8a5376ab4fd1618ed28815b3a
Garuda127 <71472029+Garuda127@users.noreply.github.com>
2021-09-30 14:45:43 -0500

    P.Media terminada
    Se termino P.Media pero no se a agregado a los demas forms

---
+++ PMedia.Designer.cs
@@ -0,0 +1,235 @@
+﻿
+namespace Plantilla_formulario
+{
+    partial class PMedia
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.PMediaTxtN = new System.Windows.Forms.TextBox();
+            this.PmediaTxtMedia = new System.Windows.Forms.TextBox();
+            this.PMediaTxt1 = new System.Windows.Forms.TextBox();
+            this.PMediaTxtZ = new System.Windows.Forms.TextBox();
+            this.PMediaTxtLi = new System.Windows.Forms.TextBox();
+            this.PMediaTxtLs = new System.Windows.Forms.TextBox();
+            this.DatosMedia = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DatosMedia)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.PMediaTxtLs);
+            this.panel1.Controls.Add(this.PMediaTxtLi);
+            this.panel1.Controls.Add(this.PMediaTxtZ);
+            this.panel1.Controls.Add(this.PMediaTxt1);
+            this.panel1.Controls.Add(this.PmediaTxtMedia);
+            this.panel1.Controls.Add(this.PMediaTxtN);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.btnExit);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(167, 436);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.btnExit.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnExit.FlatAppearance.BorderSize = 0;
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.Location = new System.Drawing.Point(0, 395);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(167, 41);
+            this.btnExit.TabIndex = 0;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "N=";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Media=";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "1-(a/2)=";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(33, 61);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(20, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Z=";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(32, 87);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(21, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Li=";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(29, 139);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(24, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Ls=";
+            // 
+            // PMediaTxtN
+            // 
+            this.PMediaTxtN.Location = new System.Drawing.Point(59, 6);
+            this.PMediaTxtN.Name = "PMediaTxtN";
+            this.PMediaTxtN.Size = new System.Drawing.Size(100, 20);
+            this.PMediaTxtN.TabIndex = 7;
+            // 
+            // PmediaTxtMedia
+            // 
+            this.PmediaTxtMedia.Location = new System.Drawing.Point(59, 110);
+            this.PmediaTxtMedia.Name = "PmediaTxtMedia";
+            this.PmediaTxtMedia.Size = new System.Drawing.Size(100, 20);
+            this.PmediaTxtMedia.TabIndex = 8;
+            // 
+            // PMediaTxt1
+            // 
+            this.PMediaTxt1.Location = new System.Drawing.Point(59, 32);
+            this.PMediaTxt1.Name = "PMediaTxt1";
+            this.PMediaTxt1.Size = new System.Drawing.Size(100, 20);
+            this.PMediaTxt1.TabIndex = 9;
+            // 
+            // PMediaTxtZ
+            // 
+            this.PMediaTxtZ.Location = new System.Drawing.Point(59, 58);
+            this.PMediaTxtZ.Name = "PMediaTxtZ";
+            this.PMediaTxtZ.Size = new System.Drawing.Size(100, 20);
+            this.PMediaTxtZ.TabIndex = 10;
+            // 
+            // PMediaTxtLi
+            // 
+            this.PMediaTxtLi.Location = new System.Drawing.Point(59, 84);
+            this.PMediaTxtLi.Name = "PMediaTxtLi";
+            this.PMediaTxtLi.Size = new System.Drawing.Size(100, 20);
+            this.PMediaTxtLi.TabIndex = 11;
+            // 
+            // PMediaTxtLs
+            // 
+            this.PMediaTxtLs.Location = new System.Drawing.Point(59, 136);
+            this.PMediaTxtLs.Name = "PMediaTxtLs";
+            this.PMediaTxtLs.Size = new System.Drawing.Size(100, 20);
+            this.PMediaTxtLs.TabIndex = 12;
+            // 
+            // DatosMedia
+            // 
+            this.DatosMedia.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DatosMedia.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1});
+            this.DatosMedia.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.DatosMedia.Location = new System.Drawing.Point(167, 0);
+            this.DatosMedia.Name = "DatosMedia";
+            this.DatosMedia.Size = new System.Drawing.Size(175, 436);
+            this.DatosMedia.TabIndex = 1;
+            // 
+            // Column1
+            // 
+            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column1.HeaderText = "Ri";
+            this.Column1.Name = "Column1";
+            // 
+            // PMedia
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(342, 436);
+            this.Controls.Add(this.DatosMedia);
+            this.Controls.Add(this.panel1);
+            this.Name = "PMedia";
+            this.Text = "PMedia";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DatosMedia)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        public System.Windows.Forms.TextBox PMediaTxtLs;
+        public System.Windows.Forms.TextBox PMediaTxtLi;
+        public System.Windows.Forms.TextBox PMediaTxtZ;
+        public System.Windows.Forms.TextBox PMediaTxt1;
+        public System.Windows.Forms.TextBox PmediaTxtMedia;
+        public System.Windows.Forms.TextBox PMediaTxtN;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnExit;
+        public System.Windows.Forms.DataGridView DatosMedia;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+    }
+}
